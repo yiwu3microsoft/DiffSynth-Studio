93fb4a3c8dfc666b34912d6999fa918406d07fbc
yiwu3microsoft <yiwu3@microsoft.com>
2025-11-15 19:12:44 -0800

    remove offload

--- test_ywu/Qwen-Image-Edit-2509.py
+++ test_ywu/Qwen-Image-Edit-2509.py
@@ -3,16 +3,16 @@ from PIL import Image
 import torch
 from diffusers.utils import load_image
 
-pipe = QwenImagePipeline.from_pretrained(
-    torch_dtype=torch.bfloat16,
-    device="cuda:0",
-    model_configs=[
-        ModelConfig(model_id="Qwen/Qwen-Image-Edit-2509", origin_file_pattern="transformer/diffusion_pytorch_model*.safetensors", offload_device="cpu", offload_dtype=torch.float8_e4m3fn),
-        ModelConfig(model_id="Qwen/Qwen-Image", origin_file_pattern="text_encoder/model*.safetensors", offload_device="cpu", offload_dtype=torch.float8_e4m3fn),
-        ModelConfig(model_id="Qwen/Qwen-Image", origin_file_pattern="vae/diffusion_pytorch_model.safetensors", offload_device="cpu", offload_dtype=torch.float8_e4m3fn),
-    ],
-    processor_config=ModelConfig(model_id="Qwen/Qwen-Image-Edit", origin_file_pattern="processor/"),
-)
+# pipe = QwenImagePipeline.from_pretrained(
+#     torch_dtype=torch.bfloat16,
+#     device="cuda:0",
+#     model_configs=[
+#         ModelConfig(model_id="Qwen/Qwen-Image-Edit-2509", origin_file_pattern="transformer/diffusion_pytorch_model*.safetensors", offload_device="cpu", offload_dtype=torch.float8_e4m3fn),
+#         ModelConfig(model_id="Qwen/Qwen-Image", origin_file_pattern="text_encoder/model*.safetensors", offload_device="cpu", offload_dtype=torch.float8_e4m3fn),
+#         ModelConfig(model_id="Qwen/Qwen-Image", origin_file_pattern="vae/diffusion_pytorch_model.safetensors", offload_device="cpu", offload_dtype=torch.float8_e4m3fn),
+#     ],
+#     processor_config=ModelConfig(model_id="Qwen/Qwen-Image-Edit", origin_file_pattern="processor/"),
+# )
 # pipe.enable_vram_management()
 
 # image_1 = pipe(prompt="一位少女", seed=0, num_inference_steps=40, height=1328, width=1024) # 43GB, 03:08
@@ -25,6 +25,17 @@ pipe = QwenImagePipeline.from_pretrained(
 # edit_image = [Image.open("image1.jpg"), Image.open("image2.jpg")]
 # image_3 = pipe(prompt, edit_image=edit_image, seed=1, num_inference_steps=40, height=1328, width=1024, edit_image_auto_resize=True) # 44GB, 06:17
 # image_3.save("image3.jpg")
+
+pipe = QwenImagePipeline.from_pretrained(
+    torch_dtype=torch.bfloat16,
+    device="cuda:0",
+    model_configs=[
+        ModelConfig(model_id="Qwen/Qwen-Image-Edit-2509", origin_file_pattern="transformer/diffusion_pytorch_model*.safetensors"),
+        ModelConfig(model_id="Qwen/Qwen-Image", origin_file_pattern="text_encoder/model*.safetensors"),
+        ModelConfig(model_id="Qwen/Qwen-Image", origin_file_pattern="vae/diffusion_pytorch_model.safetensors"),
+    ],
+    processor_config=ModelConfig(model_id="Qwen/Qwen-Image-Edit", origin_file_pattern="processor/"),
+)
 
 image1 = load_image("https://huggingface.co/datasets/nunchaku-tech/test-data/resolve/main/inputs/man.png")
 image1 = image1.convert("RGB")
